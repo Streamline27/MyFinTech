cae799de17b4fb0623fa52b5f64d7be6efefb5cb
Vladislav <vladislav.ykshn@gmail.com>
2020-12-12 20:19:45 +0200

    Added archunit tests

--- currency-converter/src/main/java/lv/forfun/currencyconverter/domain/ExchangeFeeRepository.java
+++ currency-converter/src/main/java/lv/forfun/currencyconverter/domain/ExchangeFeeRepository.java
@@ -1,10 +1,12 @@
 package lv.forfun.currencyconverter.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ExchangeFeeRepository extends CrudRepository<ExchangeFee, Long> {
 
     Optional<ExchangeFee> findByFromAndTo(String from, String to);
--- currency-converter/src/test/java/lv/forfun/currencyconverter/ArchitecturalConventionTests.java
+++ currency-converter/src/test/java/lv/forfun/currencyconverter/ArchitecturalConventionTests.java
@@ -6,13 +6,14 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import com.tngtech.archunit.lang.syntax.elements.ClassesShouldConjunction;
 import lv.forfun.currencyconverter.api.Regex;
+import org.springframework.data.repository.Repository;
 
 
 @AnalyzeClasses(packages = "lv.forfun.currencyconverter")
-public class ArchUnitTestDemo {
+public class ArchitecturalConventionTests {
 
     @ArchTest
-    void apiClassesShouldEndWithRequestOrResponseOrDto(JavaClasses classes) {
+    void apiClassesShouldFollowConventions(JavaClasses classes) {
         ClassesShouldConjunction rule = ArchRuleDefinition.classes()
                 .that().resideInAPackage("..api..")
                 .and().areNotNestedClasses()
@@ -21,6 +22,16 @@ public class ArchUnitTestDemo {
                 .should().haveSimpleNameEndingWith("Request")
                 .orShould().haveSimpleNameEndingWith("Response")
                 .orShould().haveSimpleNameEndingWith("Dto");
+        rule.check(classes);
+    }
+
+    @ArchTest
+    void repositoryClassesShouldFollowConventions(JavaClasses classes) {
+        ClassesShouldConjunction rule = ArchRuleDefinition.classes()
+                .that().areAssignableTo(Repository.class)
+                .should().beAnnotatedWith(org.springframework.stereotype.Repository.class)
+                .andShould().haveSimpleNameEndingWith("Repository")
+                .andShould().resideInAnyPackage("lv.forfun.currencyconverter.domain..");
         rule.check(classes);
     }
 }
