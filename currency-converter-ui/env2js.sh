2610edd0889de64161f11a829daf3ef6c363c7aa
Vladislav <vladislav.ykshn@gmail.com>
2020-12-21 18:08:26 +0200

    Execute scrip to modify env variables

--- currency-converter-ui/env2js.sh
+++ currency-converter-ui/env2js.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -n "$API_HOST" ]; then
-  echo "window.RUNTIME_API_HOST = '$API_HOST'" > public/env.js;
+  echo "window.RUNTIME_API_HOST = '$API_HOST'" > /usr/share/nginx/html/env.js;
 fi
