0c15b37c56ec40e21ad7f494f0ef8d01fde982e8
Vladislav <vladislav.ykshn@gmail.com>
2020-12-22 16:17:23 +0200

    Singe gradle for all project

--- currency-converter/docker-build.sh
+++ currency-converter/docker-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-./gradlew build -x test
+# Get .sh script directiory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+$DIR/../gradlew :currency-converter:build -x test
 docker build . -t currency-converter.app:latest
